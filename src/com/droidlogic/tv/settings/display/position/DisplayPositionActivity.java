66d959139185f99ab4f53b9150d0e837ce2d84de
Jinping Wang <jinping.wang@amlogic.com>
2017-04-14 11:18:02 +0800

    PD #142577: add a border to see clear when zoom in or out
    Change-Id: I92bf7222f0966c9ec8a30c626a44f4c7abe7471c

--- src/com/droidlogic/tv/settings/display/position/DisplayPositionActivity.java
+++ src/com/droidlogic/tv/settings/display/position/DisplayPositionActivity.java
@@ -22,69 +22,76 @@ import com.droidlogic.tv.settings.dialog.old.ActionAdapter;
 import com.droidlogic.tv.settings.dialog.old.ActionFragment;
 import com.droidlogic.tv.settings.dialog.old.ContentFragment;
 import com.droidlogic.tv.settings.dialog.old.DialogActivity;
+import android.widget.FrameLayout;
 
 public class DisplayPositionActivity extends DialogActivity implements ActionAdapter.Listener {
-	private static final String zoom_in = "zoom in";
-	private static final String zoom_out = "zoom out";
+    private static final String zoom_in = "zoom in";
+    private static final String zoom_out = "zoom out";
 
-	private ContentFragment mContentFragment;
-	private ActionFragment mActionFragment;
-	private DisplayPositionManager mDisplayPositionManager;
+    private ContentFragment mContentFragment;
+    private ActionFragment mActionFragment;
+    private DisplayPositionManager mDisplayPositionManager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout border= (FrameLayout)findViewById(R.id.border_fragment);
+        border.setVisibility(View.VISIBLE);
 
-		mDisplayPositionManager = new DisplayPositionManager(this);
-		mContentFragment = createMainMenuContentFragment();
-		mActionFragment = ActionFragment.newInstance(getMainActions());
-		setContentAndActionFragments(mContentFragment, mActionFragment);
-	}
+        mDisplayPositionManager = new DisplayPositionManager(this);
+        mContentFragment = createMainMenuContentFragment();
+        mActionFragment = ActionFragment.newInstance(getMainActions());
+        setContentAndActionFragments(mContentFragment, mActionFragment);
+    }
 
-	@Override
-	protected void onPause() {
-		mDisplayPositionManager.saveDisplayPosition();
-		super.onPause();
-	}
+    @Override
+    protected void onPause() {
+        mDisplayPositionManager.saveDisplayPosition();
+        super.onPause();
+    }
 
-	@Override
-	public void onActionClicked(Action action) {
-		String mode = action.getKey();
+    @Override
+    public void onActionClicked(Action action) {
+        String mode = action.getKey();
 
-		if (mode.equals(zoom_in)) {
-			mDisplayPositionManager.zoomIn();
-		} else {
-			mDisplayPositionManager.zoomOut();
-		}
-		updateMainScreen();
-	}
+        if (mode.equals(zoom_in)) {
+            mDisplayPositionManager.zoomIn();
+        } else {
+            mDisplayPositionManager.zoomOut();
+        }
+        updateMainScreen();
+    }
 
-	private void goToMainScreen() {
-		updateMainScreen();
-		getFragmentManager().popBackStack(null, 0);
-	}
+    private void goToMainScreen() {
+        updateMainScreen();
+        getFragmentManager().popBackStack(null, 0);
+    }
 
-	private void updateMainScreen() {
-		mContentFragment.setDescriptionText(getString(R.string.device_position_description) + " "
-				+ mDisplayPositionManager.getCurrentRateValue() + "%");
-	}
+    private void updateMainScreen() {
+        mContentFragment.setDescriptionText(
+            getString(R.string.device_position_description) + " " + mDisplayPositionManager.getCurrentRateValue()+ "%");
+    }
 
-	private ContentFragment createMainMenuContentFragment() {
-		return ContentFragment.newInstance(getString(R.string.device_position),
-				getString(R.string.device_display), getString(R.string.device_position_description) + " "
-						+ mDisplayPositionManager.getInitialRateValue() + "%",
-				R.drawable.ic_settings_overscan, getResources().getColor(R.color.icon_background));
-	}
+    private ContentFragment createMainMenuContentFragment() {
+        return ContentFragment.newInstance(
+                getString(R.string.device_position), getString(R.string.device_display),
+                getString(R.string.device_position_description) + " " + mDisplayPositionManager.getInitialRateValue()+ "%",
+                R.drawable.ic_settings_overscan,
+                getResources().getColor(R.color.icon_background));
+    }
 
-	private ArrayList<Action> getMainActions() {
-		ArrayList<Action> actions = new ArrayList<Action>();
+    private ArrayList<Action> getMainActions() {
+        ArrayList<Action> actions = new ArrayList<Action>();
 
-		actions.add(new Action.Builder().key(zoom_in)
-				.title("               " + getString(R.string.device_position_zoomin)).build());
+        actions.add(new Action.Builder().key(zoom_in)
+            .title("               "+getString(R.string.device_position_zoomin))
+            .build());
 
-		actions.add(new Action.Builder().key(zoom_out)
-				.title("               " + getString(R.string.device_position_zoomout)).build());
+        actions.add(new Action.Builder().key(zoom_out)
+            .title("               "+getString(R.string.device_position_zoomout))
+            .build());
 
-		return actions;
-	}
+        return actions;
+    }
 }
+
