0554532cfb47c390e67e19fbffe7f44cfb9f4712
Jinping Wang <jinping.wang@amlogic.com>
2017-07-28 18:52:07 +0800

    DroidTvSettings:optimise deepcolor display layer[1/1]
    PD# 147298
    
    use fragment instead of activity
    
    Change-Id: I9fadf70930401546f12ecfb6a252bd32c4f98f49

---
+++ src/com/droidlogic/tv/settings/display/outputmode/ColorAttributeFragment.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.droidlogic.tv.settings.display.outputmode;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Keep;
+import android.support.v17.preference.LeanbackPreferenceFragment;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+import com.droidlogic.tv.settings.dialog.old.Action;
+import com.droidlogic.tv.settings.RadioPreference;
+import com.droidlogic.tv.settings.R;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+@Keep
+public class ColorAttributeFragment extends LeanbackPreferenceFragment {
+    private static final String LOG_TAG = "ColorAttributeFragment";
+    private OutputUiManager mOutputUiManager;
+    private static String saveValue = null;
+    private static String curValue = null;
+    private static String curMode = null;
+    private static final int MSG_FRESH_UI = 0;
+    private IntentFilter mIntentFilter;
+    private static final String DEFAULT_VALUE = "444,8bit";
+    private static final String DEFAULT_TITLE = "YCbCr444 8bit";
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final Activity activity = getActivity();
+            if (activity != null) {
+                mHandler.sendEmptyMessageDelayed(MSG_FRESH_UI, 1000);
+            }
+        }
+    };
+    public static ColorAttributeFragment newInstance() {
+        return new ColorAttributeFragment();
+    }
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        mOutputUiManager = new OutputUiManager(getActivity());
+        mIntentFilter = new IntentFilter("android.intent.action.HDMI_PLUGGED");
+        final Context themedContext = getPreferenceManager().getContext();
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(
+                themedContext);
+        screen.setTitle(R.string.device_formatcolor);
+        setPreferenceScreen(screen);
+        final List<Action> InfoList = getMainActions();
+        for (final Action Info : InfoList) {
+            final String InfoTag = Info.getKey();
+            final RadioPreference radioPreference = new RadioPreference(themedContext);
+            radioPreference.setKey(InfoTag);
+            radioPreference.setPersistent(false);
+            radioPreference.setTitle(Info.getTitle());
+            radioPreference.setLayoutResource(R.layout.preference_reversed_widget);
+            if (Info.isChecked()) {
+                radioPreference.setChecked(true);
+            }
+            screen.addPreference(radioPreference);
+        }
+        //getActivity().registerReceiver(mIntentReceiver, mIntentFilter);
+    }
+    private boolean isModeSupportColor(final String curMode, final String curValue){
+        boolean  ret = false;
+        ret = mOutputUiManager.isModeSupportColor(curMode, curValue);
+        return ret;
+    }
+
+    private ArrayList<Action> getMainActions() {
+        ArrayList<Action> actions = new ArrayList<Action>();
+        ArrayList<String> colorTitleList = mOutputUiManager.getColorTitleList();
+        ArrayList<String> colorValueList = mOutputUiManager.getColorValueList();
+        String value = null;
+        String  curColorValue = mOutputUiManager.getCurrentColorAttribute().toString().trim();
+        Log.i(LOG_TAG,"curColorValue: "+curColorValue);
+        if (curColorValue.equals("default"))
+            curColorValue = DEFAULT_VALUE;
+        for (int i = 0; i < colorTitleList.size(); i++) {
+            value = colorValueList.get(i).trim();
+            curMode = mOutputUiManager.getCurrentMode().trim();
+            if (!isModeSupportColor(curMode, value)) {
+                continue;
+            }
+
+            if (curColorValue.equals(value)) {
+                actions.add(new Action.Builder().key(value)
+                        .title("        " + colorTitleList.get(i))
+                        .checked(true).build());
+            }else {
+                actions.add(new Action.Builder().key(value)
+                        .title("        " + colorTitleList.get(i))
+                        .description("").build());
+            }
+        }
+        if (actions.size() == 0) {
+            actions.add(new Action.Builder().key(DEFAULT_VALUE)
+                .title("        " + DEFAULT_TITLE)
+                .checked(true).build());
+        }
+        return actions;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //getActivity().unregisterReceiver(mIntentReceiver);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference instanceof RadioPreference) {
+            final RadioPreference radioPreference = (RadioPreference) preference;
+            radioPreference.clearOtherRadioPreferences(getPreferenceScreen());
+            if (radioPreference.isChecked()) {
+                if (onClickHandle(radioPreference.getKey()) == true) {
+                    radioPreference.setChecked(true);
+                }
+            } else {
+            Log.i(LOG_TAG,"not checked");
+            }
+		    }
+      return super.onPreferenceTreeClick(preference);
+    }
+    public boolean onClickHandle(String key) {
+        curValue= key;
+        saveValue= mOutputUiManager.getCurrentColorAttribute().toString().trim();
+        if (saveValue.equals("default"))
+            saveValue = DEFAULT_VALUE;
+        curMode = mOutputUiManager.getCurrentMode().trim();
+        Log.i(LOG_TAG,"curValue: "+curValue + "saveValue: "+saveValue);
+        if (!curValue.equals(saveValue)) {
+           if (isModeSupportColor(curMode,curValue)) {
+               mOutputUiManager.changeColorAttribte(curValue);
+               return true;
+           }
+           else{
+               String msg  = "Not support this color format & depth!";
+               Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT);
+               toast.show();
+               return false;
+           }
+        }
+        return false;
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_FRESH_UI:
+                    break;
+            }
+        }
+    };
+}
---
+++ src/com/droidlogic/tv/settings/display/outputmode/ScreenResolutionFragment.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.droidlogic.tv.settings.display.outputmode;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v17.preference.LeanbackPreferenceFragment;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
+
+import com.droidlogic.tv.settings.R;
+
+
+
+public class ScreenResolutionFragment extends LeanbackPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_DEEPCOLOR = "deepcolor_setting";
+    private static final String KEY_DISPLAYMODE = "displaymode_setting";
+    private static final String KEY_BEST_RESOLUTION = "best_resolution";
+    private static final String DEFAULT_VALUE = "444,8bit";
+
+    private Preference mBestResolutionPref;
+    private Preference mDisplayModePref;
+    private Preference mDeepColorPref;
+    private OutputUiManager mOutputUiManager;
+    private IntentFilter mIntentFilter;
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final Activity activity = getActivity();
+            if (activity != null) {
+                updateScreenResolutionDisplay(activity);
+            }
+        }
+    };
+
+    public static ScreenResolutionFragment newInstance() {
+        return new ScreenResolutionFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOutputUiManager = new OutputUiManager(getActivity());
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.screen_resolution, null);
+
+        mBestResolutionPref = findPreference(KEY_BEST_RESOLUTION);
+        mBestResolutionPref.setOnPreferenceChangeListener(this);
+        mDisplayModePref = findPreference(KEY_DISPLAYMODE);
+        mDeepColorPref = findPreference(KEY_DEEPCOLOR);
+        mIntentFilter = new IntentFilter("android.intent.action.HDMI_PLUGGED");
+        getActivity().registerReceiver(mIntentReceiver, mIntentFilter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateScreenResolutionDisplay(getActivity());
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unregisterReceiver(mIntentReceiver);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void updateScreenResolutionDisplay(Context context) {
+        ((SwitchPreference)mBestResolutionPref).setChecked(isBestResolution());
+        if (isBestResolution()) {
+           mBestResolutionPref.setSummary(R.string.captions_display_on);
+        }else {
+           mBestResolutionPref.setSummary(R.string.captions_display_off);
+        }
+        mDisplayModePref.setSummary(getCurrentDisplayMode());
+        mDeepColorPref.setSummary(getCurrentDeepColor());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (TextUtils.equals(preference.getKey(), KEY_BEST_RESOLUTION)) {
+            setBestResolution();
+            updateScreenResolutionDisplay(getActivity());
+        }
+        return true;
+    }
+    private boolean isBestResolution() {
+        return mOutputUiManager.isBestOutputmode();
+    }
+    private void setBestResolution() {
+        mOutputUiManager.change2BestMode();
+    }
+    private String getCurrentDisplayMode() {
+        return mOutputUiManager.getCurrentMode().trim();
+    }
+    private String getCurrentDeepColor() {
+        String value = mOutputUiManager.getCurrentColorAttribute().toString().trim();
+        if (value.equals("default") || value == "" || value.equals(""))
+            return DEFAULT_VALUE;
+        return value;
+    }
+}
