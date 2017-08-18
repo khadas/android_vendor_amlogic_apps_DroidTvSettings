eb6ba6b8cea53a15b3bcc0cb6dde4522f56263c7
Luan Yuan <luan.yuan@amlogic.com>
2017-08-18 18:49:40 +0800

    DroidTvSettings: modify Display Position Style.[1/1]
    PD# 149534
    
    modify uniform style interface.
    
    Change-Id: Ieec9853fd0bd0ed584aa369f2d4611776e61b80d
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- src/com/droidlogic/tv/settings/display/position/DisplayPositionActivity.java
+++ src/com/droidlogic/tv/settings/display/position/DisplayPositionActivity.java
@@ -1,97 +1,42 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.droidlogic.tv.settings.display.position;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import android.app.Fragment;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import com.droidlogic.tv.settings.BaseSettingsFragment;
+import com.droidlogic.tv.settings.TvSettingsActivity;
 
-import com.droidlogic.app.DisplayPositionManager;
-import com.droidlogic.tv.settings.R;
-import com.droidlogic.tv.settings.dialog.old.Action;
-import com.droidlogic.tv.settings.dialog.old.ActionAdapter;
-import com.droidlogic.tv.settings.dialog.old.ActionFragment;
-import com.droidlogic.tv.settings.dialog.old.ContentFragment;
-import com.droidlogic.tv.settings.dialog.old.DialogActivity;
-import android.widget.FrameLayout;
-
-public class DisplayPositionActivity extends DialogActivity implements ActionAdapter.Listener {
-    private static final String zoom_in = "zoom in";
-    private static final String zoom_out = "zoom out";
-
-    private ContentFragment mContentFragment;
-    private ActionFragment mActionFragment;
-    private DisplayPositionManager mDisplayPositionManager;
+public class DisplayPositionActivity extends TvSettingsActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FrameLayout border= (FrameLayout)findViewById(R.id.border_fragment);
-        border.setVisibility(View.VISIBLE);
-
-        mDisplayPositionManager = new DisplayPositionManager(this);
-        mContentFragment = createMainMenuContentFragment();
-        mActionFragment = ActionFragment.newInstance(getMainActions());
-        setContentAndActionFragments(mContentFragment, mActionFragment);
+    protected Fragment createSettingsFragment() {
+        return DisplayPositionFragment.newInstance();
     }
 
-    @Override
-    protected void onPause() {
-        mDisplayPositionManager.saveDisplayPosition();
-        super.onPause();
-    }
+    public static class SettingsFragment extends BaseSettingsFragment {
 
-    @Override
-    public void onActionClicked(Action action) {
-        String mode = action.getKey();
-
-        if (mode.equals(zoom_in)) {
-            mDisplayPositionManager.zoomIn();
-        } else {
-            mDisplayPositionManager.zoomOut();
+        public static SettingsFragment newInstance() {
+            return new SettingsFragment();
         }
-        updateMainScreen();
-    }
 
-    private void goToMainScreen() {
-        updateMainScreen();
-        getFragmentManager().popBackStack(null, 0);
-    }
-
-    private void updateMainScreen() {
-        mContentFragment.setDescriptionText(
-            getString(R.string.device_position_description) + " " + mDisplayPositionManager.getCurrentRateValue()+ "%");
-    }
-
-    private ContentFragment createMainMenuContentFragment() {
-        return ContentFragment.newInstance(
-                getString(R.string.device_position), getString(R.string.device_display),
-                getString(R.string.device_position_description) + " " + mDisplayPositionManager.getInitialRateValue()+ "%",
-                R.drawable.ic_settings_overscan,
-                getResources().getColor(R.color.icon_background));
-    }
-
-    private ArrayList<Action> getMainActions() {
-        ArrayList<Action> actions = new ArrayList<Action>();
-
-        actions.add(new Action.Builder().key(zoom_in)
-            .title("               "+getString(R.string.device_position_zoomin))
-            .build());
-
-        actions.add(new Action.Builder().key(zoom_out)
-            .title("               "+getString(R.string.device_position_zoomout))
-            .build());
-
-        return actions;
+        @Override
+        public void onPreferenceStartInitialScreen() {
+            final DisplayPositionFragment fragment = DisplayPositionFragment.newInstance();
+            startPreferenceFragment(fragment);
+        }
     }
 }
-
