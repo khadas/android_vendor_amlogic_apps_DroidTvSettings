4cc962f3834d11a46fc1fe7850729750880e9f7f
Zhiwei Yan <zhiwei.yan@amlogic.com>
2018-08-20 15:42:09 +0800

    DroidTvSettings: constructor tvcontroldata provider [5/5]
    PD# 171673
    
    constructor tvcontroldata provider
    
    Change-Id: I1e844ada064c8fabca49cf5c0ed1f3c9b9cfb99a

--- src/com/droidlogic/tv/settings/display/dolbyvision/DvReceiver.java
+++ src/com/droidlogic/tv/settings/display/dolbyvision/DvReceiver.java
@@ -5,18 +5,38 @@ import android.content.Context;
 import android.content.Intent;
 //import android.os.Process;
 import android.widget.Toast;
+import android.util.Log;
+import android.content.ContentResolver;
+
+import com.droidlogic.app.tv.TvControlDataManager;
 
 public class DvReceiver extends BroadcastReceiver {
     //static final String ACTION = "android.intent.action.BOOT_COMPLETED";
+    private static final String TAG = "DvReceiver";
 
     @Override
     public void onReceive (Context context, Intent intent) {
         if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
             Intent serviceIntent = new Intent(context, DolbyVisionService.class);
             context.startService(serviceIntent);
+            checkTvControlDataProvider(context);
         }
     }
 
+    private void checkTvControlDataProvider(final Context context) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "checkTvControlDataProvider = " + context);
+                TvControlDataManager tvcontroldata = TvControlDataManager.getInstance(context);
+                ContentResolver content = context.getContentResolver();
+                if (!tvcontroldata.getBoolean(content, TvControlDataManager.KEY_INIT, false)) {
+                    //init the tv_control_data.db if not exist
+                    tvcontroldata.putBoolean(content, TvControlDataManager.KEY_INIT, true);
+                }
+            }
+        }).start();
+    }
 }
 
 
